050e77b46ae577fa209b3f458c966f923fc2ea9b
AndrewPBerg <andberg9@gmail.com>
2026-02-09 07:52:46 -0500

    upgraded seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
-import solidJs from "@astrojs/solid-js";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cascadinglabs.com",
   output: "static",
-  integrations: [solidJs()],
+  integrations: [sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
