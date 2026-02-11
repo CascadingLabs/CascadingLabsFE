201bb17339c24699a53f21c7d6a7da96c8941c3f
AndrewPBerg <andberg9@gmail.com>
2026-02-11 08:08:09 -0500

    added linting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cascadinglabs.com",
-  output: "static",
-  integrations: [sitemap()],
+	site: 'https://cascadinglabs.com',
+	output: 'static',
+	integrations: [sitemap()],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
